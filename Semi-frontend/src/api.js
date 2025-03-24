c449ff755219d69c90214176f4a979e364bd7dc9
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2025-03-24 23:39:05 +0530

    chore: update api

--- Semi-frontend/src/api.js
+++ Semi-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiserver = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://cropxpert-server.onrender.com'
 });
 
 export default apiserver;
